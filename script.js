3b7e4072d8068487a05627ad23dbacd81e1e5774
Hannahphoto <hannahprice67@gmail.com>
2023-10-23 18:33:45 -0400

    whatever progress

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// TODO: Add code to display the current date in the header of the page.
+var currentDayEl = $('#currentDay');
+currentDayEl.append(dayjs().format('dddd, MMMM D, YYYY Z a'));
+
+var saveBtnEL =$("saveBtn");
+
+var descriptionEl = $('description');
+
+// descriptionEl.val() = [];
+
+
+
+$(function schedule (event) {
+  // event.preventDefault();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,6 +26,12 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+  var descriptionValue = $('input[description]').val();
+  descriptionValue.text();
+    event.preventDefault();
+  
+  // descriptionEl.data();
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -19,5 +42,8 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+
+
+  saveBtnEL.on("click", schedule);
 });
+ 
